a7b00a22ffbaf746cc175ac8399950c0f626d6c4
Adrianus Kleemans <a.kleemans@gmail.com>
2022-12-11 17:55:22 +0100

    Update name & build

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/season-scheduler'),
+      dir: require('path').join(__dirname, './coverage/curling-season-scheduler'),
       subdir: '.',
       reporters: [
         { type: 'html' },
